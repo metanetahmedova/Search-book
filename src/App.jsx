26a071897f0c7dae6dd11c3625a2c876433873b3
metanetahmedova <metanetahmedova23@gmail.com>
2023-06-18 22:55:50 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/HomePageComponents/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
